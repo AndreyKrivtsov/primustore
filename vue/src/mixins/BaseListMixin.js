f456837648e27a62f4651a1c23f1d0de6de762b5
AK <andreykrivtsov@gmail.com>
2021-01-13 19:25:30 +0300

    add global style file

--- vue/src/mixins/BaseListMixin.js
+++ vue/src/mixins/BaseListMixin.js
@@ -1,3 +1,5 @@
+import '../assets/style.scss'
+
 export default {
     name: 'BaseListMixin',
 
--- vue/src/templates/ShoppingCardTemplate.vue
+++ vue/src/templates/ShoppingCardTemplate.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="list">
         <div class="list__title">{{ listHeader }}</div>
-        <div class="list__item" v-for="product in items" :key="product.id">
-            <div>{{ product.id }}</div>
-            <div>{{ product.name }}</div>
-            <div>{{ product.cost }}</div>
-            <div><button @click="deleteItem(product.id)">Удалить из корзины</button></div>
+        <div class="list__items">
+            <div v-for="product in items" :key="product.id">
+                <div>{{ product.id }}</div>
+                <div>{{ product.name }}</div>
+                <div>{{ product.cost }}</div>
+                <div><button @click="deleteItem(product.id)">Удалить из корзины</button></div>
+            </div>
         </div>
     </div>
 </template>
@@ -15,25 +17,3 @@ export default {
     name: 'ListTemplate',
 }
 </script>
-
-<style lang="scss">
-.list {
-    margin-top: 30px;
-
-    .list__title {
-        font-size: 2rem;
-        margin-bottom: 30px;
-    }
-
-    .list__item {
-        display: flex;
-        justify-content: center;
-        margin: 10px auto 0 auto;
-        width: 600px;
-
-        & div:nth-child(n) {
-            flex-grow: 1;
-        }
-    }
-}
-</style>
